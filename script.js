fad51784318f866b7bac5c6aadc51646f90e296a
abdulrahaman ridwan <abdulrahamanr657@gmail.com>
2025-04-28 09:55:54 -0700

    wrote the structure for the html

---
+++ script.js
@@ -0,0 +1,10 @@
+"use script";
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error loading JSON:", error);
+  });
